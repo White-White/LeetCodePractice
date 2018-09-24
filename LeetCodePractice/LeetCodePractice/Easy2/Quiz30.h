d60b416949783faaca065aeae46207af4af9bfb4
White <jinhao.zhang@icloud.com>
2018-09-24 14:32:11 +0800

    Search Insert Position

---
+++ LeetCodePractice/LeetCodePractice/Easy2/Quiz30.h
@@ -0,0 +1,14 @@
+//
+//  Quiz30.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/9/24.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef Quiz30_h
+#define Quiz30_h
+
+#include <stdio.h>
+
+#endif /* Quiz30_h */
