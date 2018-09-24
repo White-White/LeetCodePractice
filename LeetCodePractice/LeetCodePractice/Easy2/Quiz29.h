6df4cfde8578a74c2c12a831a0abd355565ebf4e
White <jinhao.zhang@icloud.com>
2018-09-24 14:27:36 +0800

    Implement strStr()

---
+++ LeetCodePractice/LeetCodePractice/Easy2/Quiz29.h
@@ -0,0 +1,14 @@
+//
+//  Quiz29.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/9/24.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef Quiz29_h
+#define Quiz29_h
+
+#include <stdio.h>
+
+#endif /* Quiz29_h */
