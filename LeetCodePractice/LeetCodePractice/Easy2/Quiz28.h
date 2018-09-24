b1560414052796d03cf6a27eab3e1249fa046604
White <jinhao.zhang@icloud.com>
2018-09-24 14:05:05 +0800

    Remove Element

---
+++ LeetCodePractice/LeetCodePractice/Easy2/Quiz28.h
@@ -0,0 +1,14 @@
+//
+//  Quiz28.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/9/24.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef Quiz28_h
+#define Quiz28_h
+
+#include <stdio.h>
+
+#endif /* Quiz28_h */
