fa326d3dc2ab3f10c5adb741060582ea03fdeeb1
白烽 <white.zjh@alibaba-inc.com>
2018-10-03 14:36:29 +0800

    LC121

---
+++ LeetCodePractice/LeetCodePractice/Easy3/LC121.h
@@ -0,0 +1,14 @@
+//
+//  LC121.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/10/3.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef LC121_h
+#define LC121_h
+
+#include <stdio.h>
+
+#endif /* LC121_h */
