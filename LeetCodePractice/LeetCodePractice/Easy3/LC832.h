c4378a2ef4b1bc46ac21bbefe17d05b0fa73685e
白烽 <white.zjh@alibaba-inc.com>
2018-10-22 11:07:32 +0800

    LC832

---
+++ LeetCodePractice/LeetCodePractice/Easy3/LC832.h
@@ -0,0 +1,14 @@
+//
+//  LC832.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/10/22.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef LC832_h
+#define LC832_h
+
+#include <stdio.h>
+
+#endif /* LC832_h */
