24e8800eb95965cf34d93bb64eaff0b8e33cdab4
白烽 <white.zjh@alibaba-inc.com>
2018-10-22 12:50:34 +0800

    LC747

---
+++ LeetCodePractice/LeetCodePractice/Easy3/LC747.c
@@ -0,0 +1,45 @@
+/*
+ In a given integer array nums, there is always exactly one largest element.
+ 
+ Find whether the largest element in the array is at least twice as much as every other number in the array.
+ 
+ If it is, return the index of the largest element, otherwise return -1.
+ 
+ Example 1:
+ 
+ Input: nums = [3, 6, 1, 0]
+ Output: 1
+ Explanation: 6 is the largest integer, and for every other number in the array x,
+ 6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
+ 
+ Example 2:
+ 
+ Input: nums = [1, 2, 3, 4]
+ Output: -1
+ Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ 
+ 
+ Note:
+ 
+ nums will have a length in the range [1, 50].
+ Every nums[i] will be an integer in the range [0, 99].
+ 
+ */
+
+#include "LC747.h"
+int dominantIndex(int* nums, int numsSize) {
+    int ret = 0;
+    int curLargest = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] >= curLargest*2) {
+            curLargest = nums[i];
+            ret = i;
+        } else if (nums[i] > curLargest/2) {
+            ret = -1;
+            curLargest = max(nums[i], curLargest);
+        }
+    }
+    
+    return ret;
+}
---
+++ LeetCodePractice/LeetCodePractice/Easy3/LC747.h
@@ -0,0 +1,14 @@
+//
+//  LC747.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/10/22.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef LC747_h
+#define LC747_h
+
+#include <stdio.h>
+
+#endif /* LC747_h */
