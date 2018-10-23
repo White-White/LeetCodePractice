6c5c935a39a87daa159b1d405b2993f75079436c
白烽 <white.zjh@alibaba-inc.com>
2018-10-23 12:40:48 +0800

    LC724

---
+++ LeetCodePractice/LeetCodePractice/Easy3/LC724.h
@@ -0,0 +1,14 @@
+//
+//  LC724.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/10/23.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef LC724_h
+#define LC724_h
+
+#include <stdio.h>
+int pivotIndex(int* nums, int numsSize);
+#endif /* LC724_h */
--- LeetCodePractice/LeetCodePractice/OCRun.m
+++ LeetCodePractice/LeetCodePractice/OCRun.m
@@ -10,7 +10,7 @@
 //oc c 测试环境
 
 #include "LC168.h"
-#include "LC746.h"
+#include "LC724.h"
 
 @implementation OCRun
 
@@ -84,7 +84,7 @@
 + (void)run {
     int* buf[3];
     
-    int tem1[] = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+    int tem1[] = {-1,-1,0,1,1,0};
     buf[0] = tem1;
     
     int tem2[] = {5,1,2,3};
@@ -96,7 +96,7 @@
     int col[] = {4,4,4};
     int ret;
     
-    minCostClimbingStairsDP(tem1, 10);
+    pivotIndex(tem1, 6);
 }
 
 @end
