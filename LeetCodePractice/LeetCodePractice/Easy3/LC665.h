d95e2bc02e00a864000c765cf07c366dceb4553d
白烽 <white.zjh@alibaba-inc.com>
2018-10-24 11:10:34 +0800

    LC665

---
+++ LeetCodePractice/LeetCodePractice/Easy3/LC665.h
@@ -0,0 +1,14 @@
+//
+//  LC665.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/10/24.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+#ifndef LC665_h
+#define LC665_h
+
+#include <stdio.h>
+
+#endif /* LC665_h */
