deef99b0aae62c1eecf1b4489dae2a35b4b603ec
白烽 <white.zjh@alibaba-inc.com>
2018-11-26 19:35:17 +0800

    LC922

---
+++ LeetCodePractice/LeetCodePractice/Easy3/LC922.h
@@ -0,0 +1,14 @@
+//
+//  LC922.h
+//  LeetCodePractice
+//
+//  Created by White on 2018/11/26.
+//  Copyright © 2018 White. All rights reserved.
+//
+
+#ifndef LC922_h
+#define LC922_h
+
+#include <stdio.h>
+
+#endif /* LC922_h */
